320aa5389ec984f41aa98194e16fc07739c079d7
心飞为你飞 <747011882@qq.com>
2020-07-20 15:07:14 +0800

    添加版本

--- health_backend/src/main/java/cn/webdai/test/BcryptTest.java
+++ health_backend/src/main/java/cn/webdai/test/BcryptTest.java
@@ -9,6 +9,6 @@ public class BcryptTest {
     public static void main(String[] args) {
         String gensalt = BCrypt.gensalt();//盐 随机加到密码 随机生成 29个字符
         String password = BCrypt.hashpw("admin", gensalt);//根据盐对密码进行加密
-        System.out.println(password);
+        System.out.println("password:"+password);
     }
 }
